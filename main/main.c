6fb59bc30002bad566a8879f2e9aa4eeb84f9518
MatheusFilipe95 <mafieu666@hotmail.com>
2023-02-06 17:53:45 -0300

    Configuração inicial client

--- main/main.c
+++ main/main.c
@@ -1,7 +1,67 @@
 #include <stdio.h>
+#include <string.h>
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
+
+#include "wifi.h"
+#include "mqtt.h"
+
+SemaphoreHandle_t conexaoWifiSemaphore;
+SemaphoreHandle_t conexaoMQTTSemaphore;
+
+void conectadoWifi(void * params)
+{
+  while(true) 
+  {
+    if(xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
+    {
+      ESP_LOGI("[Network]", "Conexao wi-fi estabelecida");
+      // Processamento Internet
+      mqtt_start();
+      ESP_LOGI("[Mqtt_broker]", "Comunicação com o broker: mqtt://164.41.98.25 iniciada");
+    }
+  }
+}
+
+void trataComunicacaoComServidor(void * params)
+{
+  char mensagem[50];
+  char JsonAtributos[200];
+  if(xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
+  {
+    while(true)
+    {
+       float temperatura = 20.0 + (float)rand()/(float)(RAND_MAX/10.0);
+       sprintf(mensagem, "{\"temperature\": %f}", temperatura);
+       mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
+
+       sprintf(JsonAtributos, "{\"quantidade de pinos\": 5,\n\"umidade\": 20}");
+       mqtt_envia_mensagem("v1/devices/me/attributes", JsonAtributos);
+       
+       vTaskDelay(3000 / portTICK_PERIOD_MS);
+    }
+  }
+}
 
 void app_main(void)
 {
-    printf("Olá mundo!");
-    fflush(stdout);
+    // Inicializa o NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+    conexaoMQTTSemaphore = xSemaphoreCreateBinary();
+    wifi_start();
+
+    xTaskCreate(&conectadoWifi,  "Conexão ao MQTT", 4096, NULL, 1, NULL);
+    xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker", 4096, NULL, 1, NULL);
 }
